4819545be074522e7ed58e96018e704b13f3276a
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-11-20 16:28:17 +0000

    Initial commit

--- main.sh
+++ main.sh
@@ -1,1 +1,27 @@
-hi
+mkdir output
+
+cp text.file output/
+
+cd output
+
+cat text.file > read.txt
+
+pwd > pwd.txt
+ls > ls.txt
+
+cp text.file copy.txt
+
+alias date
+
+date > date.txt
+
+wc -w text.file  > textcount.txt
+ps | head -n 5 > process.txt
+ip a | head -n 5 > netstat.txt
+mount | head -n 5 > mount.txt
+touch permissions.txt
+chmod 777 permissions.txt
+TESTENV1="test"
+grep -E '\b\w{3,}\b' text.file  > regex.txt
+cd ..
+
